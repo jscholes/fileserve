b854245321fe794fe69c0c8a5bb06279013606e7
James Scholes <james@jamesscholes.com>
2016-04-15 17:30:25 +0100

    Add database models

--- src/fileserve.py
+++ src/fileserve.py
@@ -3,19 +3,63 @@
 # This program is free software, licensed under the terms of the GNU General Public License (version 3 or later).
 # See the file LICENSE for more details.
 
+import os
 import os.path
 
 from flask import abort, Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 base_directory = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
+# Load the default config file
 app.config.from_pyfile(os.path.join(base_directory, 'config.cfg'))
+# Then try to load a config file for use in production
+try:
+    app.config.from_envvar('FILESERVE_CONFIG_FILE')
+except RuntimeError:
+    # Use default/development values
+    pass
+
+if not app.config.get('SQLALCHEMY_DATABASE_URI', None):
+    # Last resort, use SQLite database for development
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{0}'.format(os.path.join(base_directory, 'fileserve.db'))
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
     abort(403)
+
+
+# Models
+class File(db.Model):
+    __tablename__ = 'files'
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String, nullable=False)
+    downloads = db.relationship('FileDownload', backref='file', lazy='select')
+
+    def __repr__(self):
+        return '<File %d|%r>' % (self.id, self.path)
+
+
+class FileDownload(db.Model):
+    __tablename__ = 'downloads'
+    id = db.Column(db.Integer, primary_key=True)
+    file_id = db.Column(db.Integer, db.ForeignKey('files.id'))
+    downloaded_at = db.Column(db.DateTime, nullable=False)
+    ip_address = db.Column(db.String(45), nullable=False)
+    user_agent = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return '<FileDownload %d|%r|%r>' % (self.file_id, self.ip_address, self.downloaded_at)
+
+
+@app.route('/file/<int:id>')
+def get_file(id):
+    return 'You requested file with ID {0}'.format(id)
 
 
 if __name__ == '__main__':
