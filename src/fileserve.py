441468ce657a59332c14bed58bdd4f7ee641d1cf
James Scholes <james@jamesscholes.com>
2016-04-20 18:02:30 +0100

    Implement a basic token-based guard against counting duplicate downloads
    Download tokens are created using a base64 encoding of a file's numeric ID, the timestamp of when they were issued and a user's IP address.  This is then stored in a file-specific cookie on the user's machine.  The /download endpoint decodes these tokens and ensures that they are still valid, i.e. that the file ID and IP address match, and that the token wasn't issued too long ago.
    
    The period for which tokens are valid is configurable using the TOKEN_VALIDITY_PERIOD setting and is 600 seconds by default.  An HTTP/403 is returned if a /download endpoint is accessed without an appropriate token.

--- src/fileserve.py
+++ src/fileserve.py
@@ -3,6 +3,7 @@
 # This program is free software, licensed under the terms of the GNU General Public License (version 3 or later).
 # See the file LICENSE for more details.
 
+from base64 import urlsafe_b64decode, urlsafe_b64encode
 import datetime
 import os
 import os.path
@@ -56,6 +57,15 @@ class FileDownload(db.Model):
     def __repr__(self):
         return '<FileDownload %d|%r|%r>' % (self.file_id, self.ip_address, self.downloaded_at)
 
+    def get_token(self):
+        token = '|'.join([
+            str(self.file_id),
+            str(self.downloaded_at.timestamp()),
+            self.ip_address
+        ])
+        token = urlsafe_b64encode(token.encode('ascii', errors='ignore')).rstrip(b'=')
+        return token
+
 
 @app.route('/')
 def index():
@@ -64,33 +74,81 @@ def index():
 
 @app.route('/file/<int:id>')
 def get_file(id):
-    file = get_file(id)
-    ip_address = request.headers.get('X-Forwarded-For', request.remote_addr)
-    user_agent = request.headers.get('User-Agent', 'Unknown-User-Agent/0.0')
+    file = get_file_info(id)
+    ip_address = get_ip_address()
+    user_agent = get_user_agent()
     download_time = datetime.datetime.now()
 
     # Check for ignored user agents
     should_count = not any(agent in user_agent for agent in app.config.get('IGNORED_USER_AGENTS'))
 
+    download = FileDownload(file_id=file.id, downloaded_at=download_time, ip_address=ip_address, user_agent=user_agent)
     if should_count:
-        download = FileDownload(file_id=file.id, downloaded_at=download_time, ip_address=ip_address, user_agent=user_agent)
         db.session.add(download)
         db.session.commit()
 
     response = make_response(redirect(url_for('download_file', id=id), 303))
+    response.set_cookie(get_token_cookie_name(file.id), download.get_token(), max_age=app.config.get('TOKEN_VALIDITY_PERIOD', 600))
     return response
 
 
 @app.route('/download/<int:id>')
 def download_file(id):
-    file = get_file(id)
-    directory, filename = os.path.split(file.path)
-    return send_from_directory(directory, filename, as_attachment=True, attachment_filename=filename)
+    valid_token = False
+    file = get_file_info(id)
+    cookies = request.cookies
+    cookie_name = get_token_cookie_name(file.id)
+    if cookie_name in cookies:
+        valid_token = verify_download_token(file.id, cookies[cookie_name])
+
+    if valid_token:
+        directory, filename = os.path.split(file.path)
+        return send_from_directory(directory, filename, as_attachment=True, attachment_filename=filename)
+    else:
+        abort(403)
 
 
-def get_file(file_id):
+def get_file_info(file_id):
     file = File.query.filter_by(id=file_id).first_or_404()
     return file
+
+
+def get_ip_address():
+    return request.headers.get('X-Forwarded-For', request.remote_addr)
+
+
+def get_user_agent():
+    return request.headers.get('User-Agent', 'Unknown-User-Agent/0.0')
+
+
+def get_token_cookie_name(file_id):
+    return 'download_token{0}'.format(file_id)
+
+
+def verify_download_token(file_id, token):
+    token = urlsafe_b64decode(add_base64_padding(token)).decode('ascii', errors='ignore')
+    try:
+        token_file_id, timestamp, token_ip_address = token.split('|')
+    except ValueError:
+        return False
+
+    if token_file_id != str(file_id) or token_ip_address != get_ip_address():
+        return False
+
+    token_creation_time = float(timestamp)
+    access_time = datetime.datetime.now().timestamp()
+    delta = access_time - token_creation_time
+    if delta < 0 or delta > app.config.get('TOKEN_VALIDITY_PERIOD', 600):
+        return False
+
+    return True
+
+
+def add_base64_padding(token):
+    required_padding = 4 - len(token) % 4
+    if required_padding:
+        token += '=' * required_padding
+    return token
 
 
 if __name__ == '__main__':
