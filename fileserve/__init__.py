7ee445b8872af296061eeef47c20404b995ee03e
James Scholes <james@jamesscholes.com>
2016-05-16 16:20:25 +0100

    Add __version__

--- fileserve/__init__.py
+++ fileserve/__init__.py
@@ -2,3 +2,5 @@
 # Copyright (C) 2016 James Scholes
 # This program is free software, licensed under the terms of the GNU General Public License (version 3 or later).
 # See the file LICENSE for more details.
+
+__version__ = '0.1'
